d91ce9b365a3e5166e861fcbaa48eec2af033ad8
Frederico Gomes de Paiva <fredericogdpaiva@gmail.com>
2021-03-13 10:38:49 -0300

    challenge finished

--- src/index.js
+++ src/index.js
@@ -23,14 +23,18 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository); // tava faltando isso
+
+  return response.status(201).json(repository); // aqui tava faltando o status(201)
 });
+
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const updatedRepository = { title, url, techs } // aqui tava faltando isso
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id); // aqui tava faltando colocar o const
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -43,12 +47,15 @@ app.put("/repositories/:id", (request, response) => {
   return response.json(repository);
 });
 
+
+
+
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -57,10 +64,11 @@ app.delete("/repositories/:id", (request, response) => {
   return response.status(204).send();
 });
 
+
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  repositoryIndex = repositories.findIndex((repository) => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -68,7 +76,9 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({likes}); // aqui likes estava como string e não estava entre chaves
 });
+
+
 
 module.exports = app;
